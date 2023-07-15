2217666542ddff7e68b1e470998f2f3d9988616a
nsoluse <128208918+nsoluse@users.noreply.github.com>
2023-07-15 18:34:59 +0700

    fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'adamant'
+
+game 'rdr3'
+
+author 'n_soluse and PerryHX'
+
+description 'frontier_prone'
+
+rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
+
+client_script 'client.lua'
+
