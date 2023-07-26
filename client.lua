5732d7723e764e72410fa8e1f87f67379f1c1312
nsoluse <128208918+nsoluse@users.noreply.github.com>
2023-07-26 22:47:47 +0700

    client.lua

--- client.lua
+++ client.lua
@@ -14,7 +14,54 @@ Citizen.CreateThread(function()
 			ProneMovement()
 			DisableControlAction(0, proneKey, true) 
 			if (not IsPauseMenuActive()) then 
-				if (IsDisabledControlJustPressed(0, proneKey) and not IsPedInAnyVehicle(ped, true) and not IsPedFalling(ped) and not IsPedDiving(ped) and not IsPedInCover(ped, false) and not IsEntityInWater(ped)) then -- Various kinds of checks
+				if (IsDisabledControlJustPressed(0, proneKey) and not IsPedInAnyVehicle(ped, true) and not IsPedFalling(ped) and not IsPedDiving(ped) and not IsPedInCover(ped, false) and not IsEntityInWater(ped) and not Citizen.InvokeNative(0xA911EE21EDF69DAF, ped)) then -- Various kinds of checks
+                    if proned then
+						TaskPlayAnimAdvanced(PlayerPedId(), "mech_crawl@base", "stealth2idle", GetEntityCoords(PlayerPedId()), 0.0, 0.0, GetEntityHeading(PlayerPedId()), 1.0, -1.0, 1500, 0, 0, 0, 0)
+						proned = false
+					elseif not proned then
+						RequestAnimDict("mech_crawl@base")
+						while not HasAnimDictLoaded("mech_crawl@base") do
+							Citizen.Wait(100)
+						end
+                        if (not IsControlPressed(2, LBM) and not IsControlPressed(2, RBM)) then
+						ClearPedTasks(ped)
+						proned = true
+                        SwitchToUnarmed()
+						SetProned()
+                        end
+					end
+				end
+                if (IsDisabledControlJustPressed(0, standKey) and proned) then
+                    TaskPlayAnimAdvanced(PlayerPedId(), "mech_crawl@base", "stealth2idle", GetEntityCoords(PlayerPedId()), 0.0, 0.0, GetEntityHeading(PlayerPedId()), 1.0, -1.0, 1500, 0, 0, 0, 0)
+                    proned = false 
+                end                
+			end
+            if (IsPlayerFreeAiming(PlayerPedId()) or proned) then
+                DisablePlayerFiring(PlayerPedId(), true)
+            end
+		else
+			proned = false
+		end
+	end
+end)
+
+local proned = false 
+proneKey = 0x4CC0E2FE -- B (The button for switching to the lying position and back)
+standKey = 0x26E9DC00 -- Z (This button is needed by those who have a hotkey to cancel all animations)
+
+local LBM = 0x07CE1E61 
+local RBM = 0xF84FA74F 
+local moveDir = {fwd = false, bwd = false, lft = false, rht = false}
+
+Citizen.CreateThread(function()
+	while true do 
+		Citizen.Wait(1)
+		local ped = PlayerPedId()
+		if (DoesEntityExist(ped) and not IsPedDeadOrDying(ped)) then 
+			ProneMovement()
+			DisableControlAction(0, proneKey, true) 
+			if (not IsPauseMenuActive()) then 
+				if (IsDisabledControlJustPressed(0, proneKey) and not IsPedInAnyVehicle(ped, true) and not IsPedFalling(ped) and not IsPedDiving(ped) and not IsPedInCover(ped, false) and not IsEntityInWater(ped) and not Citizen.InvokeNative(0xA911EE21EDF69DAF, ped)) then
                     if proned then
 						TaskPlayAnimAdvanced(PlayerPedId(), "mech_crawl@base", "stealth2idle", GetEntityCoords(PlayerPedId()), 0.0, 0.0, GetEntityHeading(PlayerPedId()), 1.0, -1.0, 1500, 0, 0, 0, 0)
 						proned = false
